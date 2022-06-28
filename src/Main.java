38dba94c02ec00a51a3156357ee2875f4ab7d6b6
shulga87 <108322839+shulga87@users.noreply.github.com>
2022-06-28 17:39:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello fuckers!");
+        }
+    }
+}
