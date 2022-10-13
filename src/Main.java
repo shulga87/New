6cc38b8e7996d7c7c58deca5ddeff7186cd56984
Kirill <k.shulga87@gmail.com>
2022-10-13 15:40:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello fuckers!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello fuckers!!!");
         }
         System.out.println("Thats all falks...");
     }
